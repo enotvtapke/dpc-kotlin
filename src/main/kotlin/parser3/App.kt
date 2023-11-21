c9790dc4c707f2d125f03d8dc95ee739ab4627e4
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2023-11-21 18:36:02 +0300

    Cut failed branches experiment

--- src/main/kotlin/parser3/App.kt
+++ src/main/kotlin/parser3/App.kt
@@ -6,12 +6,15 @@ import graph.renderGraph
 private data class ResultWrapper(val node: Node, val result: Result)
 
 private operator fun String.not() = Term(this)
-operator fun Parser.times(b: Parser) = Seq(listOf(this, b))
-operator fun Seq.times(b: Parser) = Seq(this.seq + listOf(b))
+
+//operator fun Parser.times(b: Parser) = Seq(listOf(this, b))
+//operator fun Seq.times(b: Parser) = Seq(this.seq + listOf(b))
 operator fun Seq.plus(b: Seq) = Alt(listOf(this, b))
-operator fun Seq.plus(b: Parser) = Alt(listOf(this, Seq(listOf(b))))
+
+//operator fun Seq.plus(b: Parser) = Alt(listOf(this, Seq(listOf(b))))
 operator fun Alt.plus(b: Seq) = Alt(this.alts + listOf(b))
-operator fun Alt.plus(b: Parser) = Alt(this.alts + listOf(Seq(listOf(b))))
+
+//operator fun Alt.plus(b: Parser) = Alt(this.alts + listOf(Seq(listOf(b))))
 operator fun Parser.unaryMinus() = Def(this)
 
 fun fix(l: String = "self", p: (Parser) -> (Parser)) = object : Parser {
@@ -22,25 +25,30 @@ fun fix(l: String = "self", p: (Parser) -> (Parser)) = object : Parser {
 private fun step(r: List<ResultWrapper>) = r.flatMap { (prevNode, prevRes) ->
   when (prevRes) {
     is Deferred -> {
-      prevRes.parser(prevRes.remainder).map { curRes ->
+      val res = prevRes.parser(prevRes.remainder).map { curRes ->
         val curNode = Node(curRes.toString())
         prevNode.addChild(curNode)
         ResultWrapper(curNode, curRes)
       }
+      if (res.all {
+          it.result is Deferred && it.result.parser is Seq && prevRes.parser is Seq && it.result.parser.seq.first() == prevRes.parser.seq.first()
+        }) listOf() else res
     }
+
     is Immediate -> {
       listOf(ResultWrapper(prevNode, prevRes))
     }
   }
 }
 
-private fun List<ResultWrapper>.filterImmediate() = this.filter { it.result is Immediate }.map { it.result as Immediate }
+private fun List<ResultWrapper>.filterImmediate() =
+  this.filter { it.result is Immediate }.map { it.result as Immediate }
 
 fun pass(p: Parser, s: String, maxStep: Int = Int.MAX_VALUE): Immediate {
   val root = Node("root")
   var res = listOf(ResultWrapper(root, Deferred(p, s)))
   var step = 1
-  while (res.filterImmediate().all { it.remainder.isNotEmpty() }) {
+  while (res.filterImmediate().all { it.remainder.isNotEmpty() } && res.any { it.result is Deferred }) {
     println("$step ${res.map { it.result }}")
     res = step(res)
     step++
@@ -56,10 +64,10 @@ fun pass(p: Parser, s: String, maxStep: Int = Int.MAX_VALUE): Immediate {
 }
 
 fun main() {
-  val ccc = fix("C") {
-    -it * !"a" +
-    !"c"
-  }
+//  val ccc = fix("C") {
+//    -it * !"a" +
+//    !"c"
+//  }
 //  val term = fix("T") {
 //    -it * !"+" * it +
 //    -it * !"-" * it +
@@ -67,25 +75,33 @@ fun main() {
 //  }
 
 //  println(ccc)
-//  println(pass(T, "a+a-a", 5))
+  pass(T, "a+a-a", 5)
 
-  pass(CC, "ccca", 10)
+//  pass(CC, "", 10)
 
 //  println(pass(term, "a+a-a"))
 }
 
-private data object C : Parser {
-  override fun invoke(s: String) = (-this * !"c" + !"c")(s)
-}
+//private data object C : Parser {
+//  override fun invoke(s: String) = (-this * !"c" + !"c")(s)
+//}
 
 private data object CC : NonTerminal("CC") {
-  override fun checkedInvoke(s: String) = (-this * !"c" + !"c")(s)
+  override fun checkedInvoke(s: String) = (Seq(this, listOf(-this, !"c")) + Seq(this, listOf(!"c")))(s)
 }
 
 private data object T : Parser {
-  override fun invoke(s: String) = (-T * !"+" * !"a" + -T * !"-" * !"a" + !"a")(s)
+  override fun invoke(s: String) =
+    (
+            Seq(this, listOf(-T, !"+", !"a"))
+                    +
+                    Seq(this, listOf(-T, !"-", !"a"))
+                    +
+                    Seq(this, listOf(!"a"))
+            )(s)
 }
 
-private data object A : Parser {
-  override fun invoke(s: String) = (!"a")(s)
-}
+//
+//private data object A : Parser {
+//  override fun invoke(s: String) = (!"a")(s)
+//}
--- src/main/kotlin/parser3/Parsers.kt
+++ src/main/kotlin/parser3/Parsers.kt
@@ -5,18 +5,20 @@ fun interface Parser {
 }
 
 abstract class NonTerminal(val label: String) : Parser {
-  private fun step(r: List<Result>): List<Result> = r.flatMap {
-    when (it) {
-      is Deferred -> if (it.isLeftRec()) listOf(it) else step(it.parser(it.remainder))
-      is Immediate -> listOf(it)
-    }
-  }
+//  private fun step(r: List<Result>): List<Result> = r.flatMap {
+//    when (it) {
+//      is Deferred -> if (it.isLeftRec()) listOf(it) else step(it.parser(it.remainder))
+//      is Immediate -> listOf(it)
+//    }
+//  }
 
   abstract fun checkedInvoke(s: String): List<Result>
 
   override fun invoke(s: String): List<Result> {
-    val res = step(checkedInvoke(s))
-    return if (res.all { it.isLeftRec() }) listOf() else res
+//    val res = step(checkedInvoke(s))
+    val res = checkedInvoke(s)
+    return res
+//    return if (res.all { it.isLeftRec() }) listOf() else res
   }
 
   private fun Result.isLeftRec() =
@@ -40,21 +42,60 @@ data class Def(val parser: Parser) : Parser {
   override fun toString() = "[$parser]"
 }
 
+data class Id(val result: Result) : Parser {
+  override fun invoke(s: String) = listOf(result)
+  override fun toString() = "Id $result"
+}
+
 // То, что сейчас сделано в NonTerminal должно быть сделано в Seq. То есть Seq должен распознавать левую рекурсию
-data class Seq(val seq: List<Parser>, val parentNonTerm: Parser) : Parser {
+data class Seq(val parentNonTerm: Parser, val seq: List<Parser>) : Parser {
+  private fun step(r: List<Result>): List<Result> = r.flatMap {
+    when (it) {
+      is Deferred -> if (it.isLeftRec()) listOf(it) else step(it.parser(it.remainder))
+      is Immediate -> listOf(it)
+    }
+  }
+
   private fun internal(s: String, seqTail: List<Parser>): List<Result> =
     when {
       seqTail.isEmpty() -> listOf(Immediate(s))
       else -> seqTail.first()(s).flatMap {
         when (it) {
           is Immediate -> internal(it.remainder, seqTail.drop(1))
-          is Deferred -> listOf(Deferred(Seq(listOf(it.parser) + seqTail.drop(1)), it.remainder)) // здесь должен быть isLeftReq if и flatmap
+          is Deferred -> if (it.isLeftRec() || it.isLeftRecSeq()) {
+            if (it.parser is Seq) {
+              listOf(Deferred(Seq(parentNonTerm, it.parser.seq + seqTail.drop(1)), it.remainder))
+            } else {
+              listOf(Deferred(Seq(parentNonTerm, listOf(it.parser) + seqTail.drop(1)), it.remainder))
+            }
+          }
+          else step(it.parser(it.remainder)).flatMap { s ->
+            when(s) {
+              is Immediate -> internal(s.remainder, seqTail.drop(1))
+              is Deferred ->
+                if (s.parser is Seq) {
+                  listOf(Deferred(Seq(parentNonTerm, s.parser.seq + seqTail.drop(1)), s.remainder))
+                } else {
+                  listOf(Deferred(Seq(parentNonTerm, listOf(s.parser) + seqTail.drop(1)), s.remainder))
+                }
+            }
+          }
         }
       }
     }
 
-  override fun invoke(s: String) = internal(s, seq)
-  override fun toString() = seq.joinToString(" \u22B3 ")
+  private fun Result.isLeftRec() =
+    this is Deferred && parser == parentNonTerm
+
+  private fun Result.isLeftRecSeq() =
+    this is Deferred && (this.parser is Seq && this.parser.seq.first() == this@Seq.parentNonTerm)
+
+  override fun invoke(s: String): List<Result> {
+    val res = internal(s, seq)
+    return res
+//    return if (res.all { it.isLeftRecSeq() }) listOf() else res
+  }
+  override fun toString() = "(" + seq.joinToString(" \u22B3 ") + ")"
 }
 
 data class Alt(val alts: List<Seq>) : Parser {
